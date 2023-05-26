7ac0d121e77b8a21e2341367d427fbc628e7b888
Johan Giraldo Rubio <johan.giraldor@perficient.com>
2023-05-26 10:32:29 -0500

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  hero?: Hero
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.heroService.getHero(Number(id)).subscribe(
+      (hero: Hero) => {
+        this.hero = hero
+      }
+    )
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
+
+}
